42c86fc690e4c98d49e37f128f4a6b92f3178dd8
Jose <jm.diaz6620@gmail.com>
2024-10-13 12:23:36 +0200

    empezar documentacion

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const router = require('./routes/productRoutes');
 const dbConnection = require('./config/db');
 const methodOverride = require('method-override');
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json());
 app.use(methodOverride('_method'));
